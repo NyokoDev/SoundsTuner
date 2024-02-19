21b2444ff92a6d7d32bb88c7b4418a9165d41f09
NyokoDev <129089982+NyokoDev@users.noreply.github.com>
2024-02-19 03:19:01 -0600

    Progress

--- UI/Tabs/LegacyTab.cs
+++ UI/Tabs/LegacyTab.cs
@@ -44,6 +44,7 @@ namespace POAIDBOX
         private UILabel versionInfoLabel;
         public UISlider uiSlider;
         public UIPanel MainPanel;
+        public UIScrollbar Scrollbar;
         Mod ModInitializer;
 
         public Dictionary<string, UIDropDown> soundSelections = new Dictionary<string, UIDropDown>();
@@ -59,9 +60,14 @@ namespace POAIDBOX
             ModInitializer.OnModInitializing();
 
             MainPanel = UITabstrips.AddTextTab(tabStrip, "Legacy", tabIndex, out UIButton _);
+
+
             this.soundPacks = new[] { "Default", "Custom" }.Union(SoundPacksManager.instance.SoundPacks.Values.OrderBy(p => p.Name).Select(p => p.Name)).ToArray();
-            currentY += 31f;
+
             soundPackPresetDropDown = UIDropDowns.AddLabelledDropDown(MainPanel, Margin, currentY, "Sound Preset", MainPanel.width);
+            soundPackPresetDropDown.eventSelectedIndexChanged += (c, value) => SoundPackPresetDropDownSelectionChanged(value);
+
+            currentY += 31f;
             if (soundPackPresetDropDown == null)
             {
                 UnityEngine.Debug.Log("Failed to create dropdown.");
@@ -97,25 +103,105 @@ namespace POAIDBOX
 
             foreach (var tabGroup in sliders)
             {
-
+                // Add extra space between tab groups if needed
                 foreach (var group in tabGroup.Value)
                 {
+                    // Add extra space between tab groups if needed
                     foreach (var sound in group.Value)
                     {
                         this.CreateSoundSlider(sound);
-                        currentY += 30f;
+
                     }
                     // Increment currentY by a fixed amount after all sliders in the current tab group have been added
-                    currentY += 10f; // Add extra space between tab groups if needed
+                    // Add extra space between tab groups if needed
+                }
+            }
+        }
+
+        /// <summary>
+        /// Dropdown change effect.
+        /// </summary>
+        /// <param name="value"></param>
+        public void SoundPackPresetDropDownSelectionChanged(int value)
+        {
+            this.isChangingSoundPackPreset = true;
+
+            if (value == 0)
+            {
+                // Default
+
+                foreach (UIDropDown dropDown in this.soundSelections.Values)
+                    dropDown.selectedIndex = 0;
+            }
+            else if (value == 1)
+            {
+                // Custom, don't do anything here
+            }
+            else if (value >= 2)
+            {
+                // Sound pack
+                string soundPackName = this.soundPacks[value];
+                SoundPacksFileV1.SoundPack soundPack = null;
+
+
+                if (SoundPacksManager.instance.SoundPacks.TryGetValue(soundPackName, out soundPack))
+                {
+                    foreach (var dropDown in this.soundSelections)
+                    {
+                        var prefix = dropDown.Key.Substring(0, dropDown.Key.IndexOf('.'));
+                        var id = dropDown.Key.Substring(dropDown.Key.IndexOf('.') + 1);
+                        SoundPacksFileV1.Audio[] audios = null;
+                        switch (prefix)
+                        {
+                            case "Ambient":
+                                audios = soundPack.Ambients;
+                                break;
+                            case "AmbientNight":
+                                audios = soundPack.AmbientsNight;
+                                break;
+                            case "Animal":
+                                audios = soundPack.Animals;
+                                break;
+                            case "Building":
+                                audios = soundPack.Buildings;
+                                break;
+                            case "Vehicle":
+                                audios = soundPack.Vehicles;
+                                break;
+                            case "Misc":
+                                audios = soundPack.Miscs;
+                                break;
+                        }
+                        if (audios != null)
+                        {
+                            SoundPacksFileV1.Audio audio = audios.FirstOrDefault(a => a.Type == id);
+                            if (audio != null)
+                            {
+
+                                dropDown.Value.selectedValue = audio.Name;
+                            }
+                        }
+                    }
                 }
             }
         }
 
 
 
+
+
+
+        /// <summary>
+        /// Creates sound sliders on command.
+        /// </summary>
+        /// <param name="sound"></param>
         protected void CreateSoundSlider(ISound sound)
         {
+
             UIPanel mainPanel = this.MainPanel;
+
+
+
             var configuration = Mod.Settings.GetSoundsByCategoryId<string>(sound.CategoryId);
             var customAudioFiles = SoundPacksManager.instance.AudioFiles.Where(kvp => kvp.Key.StartsWith(string.Format("{0}.{1}", sound.CategoryId, sound.Id))).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
 
@@ -124,34 +210,34 @@ namespace POAIDBOX
                 volume = configuration[sound.Id].Volume;
             else
             {
-                FileDebugge
+
                 UnityEngine.Debug.LogError($"No volume configuration found for {sound.CategoryId}.{sound.Id}, using default value: {sound.DefaultVolume}");
                 volume = sound.DefaultVolume;
             }
 
             // Add UI components to the main panel
             UISlider uiSlider = mainPanel.AddUIComponent<UISlider>();
+            currentY += 6f;
             uiSlider.name = "SoundSlider";
-            uiSlider.size = new Vector2(mainPanel.width, 16);
+           
+            uiSlider.width = 120f;
             uiSlider.relativePosition = new Vector3(Margin, currentY);
             uiSlider.minValue = 0;
             uiSlider.maxValue = sound.MaxVolume;
             uiSlider.stepSize = 0.01f;
             uiSlider.value = volume;
             uiSlider.eventValueChanged += (c, v) => this.SoundVolumeChanged(sound, v);
-            currentY += 30f;
 
-            // Increment currentY after adding each element
-            currentY += 16f; // Height of the slider
+
+
 
             // Slider track.
             UISlicedSprite sliderSprite = uiSlider.AddUIComponent<UISlicedSprite>();
             sliderSprite.atlas = UITextures.InGameAtlas;
             sliderSprite.spriteName = "BudgetSlider";
             sliderSprite.size = new Vector2(uiSlider.width, 9f);
-            sliderSprite.relativePosition = new Vector2(0f, 4f);
 
-            currentY += 9f; // Height of the slider track
+
 
             // Slider thumb.
             UISlicedSprite sliderThumb = uiSlider.AddUIComponent<UISlicedSprite>();
@@ -159,13 +245,16 @@ namespace POAIDBOX
             sliderThumb.spriteName = "SliderBudget";
             uiSlider.thumbObject = sliderThumb;
 
-            currentY += 16f; // Height of the slider thumb
+
 
             UIPanel uiPanel = uiSlider.parent as UIPanel;
+            uiPanel.maximumSize = new Vector2(100000f, 100000f);
+
+
             UILabel uiLabel = uiPanel.AddUIComponent<UILabel>();
             uiLabel.text = sound.Name;
             uiLabel.autoSize = true;
-            uiLabel.anchor = UIAnchorStyle.Left | UIAnchorStyle.CenterVertical;
+
             uiLabel.relativePosition = new Vector3(0, currentY);
             currentY += uiLabel.height;
 
@@ -185,8 +274,30 @@ namespace POAIDBOX
             {
                 // Setting up the dropdown if custom audio files are present
                 uiDropDown = uiPanel.AddUIComponent<UIDropDown>();
+                currentY += 6f;
+
                 uiDropDown.items = new[] { "Default" }.Union(customAudioFiles.Select(kvp => kvp.Value.Name)).ToArray();
-                uiDropDown.height = 28;
+                uiDropDown.zOrder = 15;
+                uiDropDown.atlas = UITextures.InGameAtlas;
+                uiDropDown.normalBgSprite = "TextFieldPanel";
+                uiDropDown.disabledBgSprite = "TextFieldPanelDisabled";
+                uiDropDown.hoveredBgSprite = "TextFieldPanelHovered";
+                uiDropDown.focusedBgSprite = "TextFieldPanelHovered";
+
+                uiDropDown.foregroundSpriteMode = UIForegroundSpriteMode.Stretch;
+                uiDropDown.listBackground = "TextFieldPanel";
+                uiDropDown.itemHover = "ListItemHover";
+                uiDropDown.itemHighlight = "ListItemHighlight";
+                uiDropDown.color = Color.white;
+                uiDropDown.popupColor = Color.white;
+                uiDropDown.textColor = Color.black;
+                uiDropDown.popupTextColor = Color.black;
+                uiDropDown.disabledColor = Color.black;
+                uiDropDown.font = UIFonts.SemiBold;
+
+
+                uiDropDown.height = 30f;
+                uiDropDown.width = 80f;
                 uiDropDown.textFieldPadding.top = 4;
                 if (configuration.ContainsKey(sound.Id) && !string.IsNullOrEmpty(configuration[sound.Id].SoundPack))
                     uiDropDown.selectedValue = configuration[sound.Id].SoundPack;
@@ -196,24 +307,23 @@ namespace POAIDBOX
                 uiDropDown.eventSelectedIndexChanged += (c, i) => this.SoundPackChanged(sound, i > 0 ? ((UIDropDown)c).items[i] : null, uiSlider);
                 this.soundSelections[string.Format("{0}.{1}", sound.CategoryId, sound.Id)] = uiDropDown;
 
-                currentY += uiDropDown.height; // Increment currentY after adding the dropdown
+
             }
 
             // Configure UI components
             uiPanel.autoLayout = false;
-            uiSlider.anchor = UIAnchorStyle.CenterVertical;
-            uiSlider.width = 207;
-            uiSlider.relativePosition = new Vector3(uiLabel.relativePosition.x + uiLabel.width + 20, 0);
+            uiSlider.width = 120f;
             if (customAudioFiles.Count > 0)
             {
-                uiDropDown.anchor = UIAnchorStyle.CenterVertical;
-                uiDropDown.width = 180;
+
+                uiDropDown.width = 80f;
+                currentY += 6f;
                 uiDropDown.relativePosition = new Vector3(uiSlider.relativePosition.x + uiSlider.width + 20, 0);
-                uiPanel.size = new Vector2(uiDropDown.relativePosition.x + uiDropDown.width, currentY);
+             
             }
             else
             {
-                uiPanel.size = new Vector2(uiSlider.relativePosition.x + uiSlider.width, currentY);
+                uiPanel.size = new Vector2(100000f, 100000f);
             }
         }
 
